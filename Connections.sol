71653bf5b212338e0251afb1456f4f9ff4610c21
homoiconicity <homoiconicity@outlook.com>
2019-05-06 14:06:59 +0800

    refactor(Connections): store hash of connections only

--- Connections.sol
+++ Connections.sol
@@ -2,14 +2,14 @@ pragma solidity >=0.4.21 <0.6.0;
 
 contract Connections {
     address public lord;
-    mapping (address => mapping (bytes => bool)) connections;
+    mapping (address => mapping (bytes32 => bool)) connections;
 
     constructor() public {
         lord = msg.sender;
     }
 
     function connect(bytes32 fromOrg, bytes32 toOrg, address toMember) public {
-        bytes memory connection = abi.encodePacked(fromOrg, toOrg, toMember);
+        bytes32 connection = makeConnection(fromOrg, toOrg, toMember);
 
         require(!connections[msg.sender][connection], "Duplicate connection.");
 
@@ -17,7 +17,7 @@ contract Connections {
     }
 
     function disconnect(bytes32 fromOrg, bytes32 toOrg, address toMember) public {
-        bytes memory connection = abi.encodePacked(fromOrg, toOrg, toMember);
+        bytes32 connection = makeConnection(fromOrg, toOrg, toMember);
 
         require(connections[msg.sender][connection], "Connection does not exist.");
 
@@ -28,7 +28,13 @@ contract Connections {
         public view
         returns (bool)
     {
-        bytes memory connection = abi.encodePacked(fromOrg, toOrg, toMember);
-        return connections[msg.sender][connection];
+        return connections[msg.sender][makeConnection(fromOrg, toOrg, toMember)];
+    }
+
+    function makeConnection(bytes32 fromOrg, bytes32 toOrg, address toMember)
+        internal pure
+        returns (bytes32)
+    {
+        return keccak256(abi.encodePacked(fromOrg, toOrg, toMember));
     }
 }
